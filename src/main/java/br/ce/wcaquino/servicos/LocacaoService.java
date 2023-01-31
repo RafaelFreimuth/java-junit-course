4c12cf8d7830c794cfaa0e3950dda0e35318108f
RafaelFreimuth <rafaelfreimuth@outlook.com>
2023-01-30 21:08:05 -0300

    ajustando método de locacao para receber um list de filmes

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -3,6 +3,7 @@ package br.ce.wcaquino.servicos;
 import static br.ce.wcaquino.utils.DataUtils.adicionarDias;
 
 import java.util.Date;
+import java.util.List;
 
 import br.ce.wcaquino.entidades.Filme;
 import br.ce.wcaquino.entidades.Locacao;
@@ -10,21 +11,42 @@ import br.ce.wcaquino.entidades.Usuario;
 
 public class LocacaoService {
 	
-	public Locacao alugarFilme(Usuario usuario, Filme filme) {
-		if (filme.getEstoque().equals(0)) {
-			throw new RuntimeException("Filme sem estoque");
+	public Locacao alugarFilme(Usuario usuario, List<Filme> filmes) {
+		
+		if (filmes == null || filmes.isEmpty()) {
+			throw new RuntimeException("Não foram passados filmes para locação");
 		}
 		
+		validarEstoqueDosFilmes(filmes);
+		
 		Locacao locacao = new Locacao();
-		locacao.setFilme(filme);
+		locacao.setFilmes(filmes);
 		locacao.setUsuario(usuario);
 		locacao.setDataLocacao(new Date());
-		locacao.setValor(filme.getPrecoLocacao());
+		locacao.setValor(obterValorTotalLocacao(filmes));
 
 		Date dataEntrega = new Date();
 		dataEntrega = adicionarDias(dataEntrega, 1);
 		locacao.setDataRetorno(dataEntrega);
 		
 		return locacao;
+	}
+
+	private void validarEstoqueDosFilmes(List<Filme> filmes) {
+		for (Filme filme : filmes) {
+			if (filme.getEstoque().equals(0)) {
+				throw new RuntimeException("Filme sem estoque");
+			}
+		}
+	}
+	
+	private Double obterValorTotalLocacao(List<Filme> filmes) {
+		Double totalizador = 0.0;
+		
+		for (Filme filme : filmes) {
+			totalizador = totalizador + filme.getPrecoLocacao();
+		}
+		
+		return totalizador;
 	}
 }
